f02304ecce920e119913a893881bc9bca43ef7de
Sam Pullara <spullara@yahoo.com>
2013-07-26 11:31:20 -0700

    add a new api for convenience

--- src/main/java/com/sampullara/cli/Args.java
+++ src/main/java/com/sampullara/cli/Args.java
@@ -15,6 +15,25 @@ import java.lang.reflect.*;
 import java.util.*;
 
 public class Args {
+
+    /**
+     * A convenience method for parsing and automatically producing error messages.
+     *
+     * @param target Either an instance or a class
+     * @param args   The arguments you want to parse and populate
+     * @return The list of arguments that were not consumed
+     */
+    public static List<String> parseOrExit(Object target, String[] args) {
+        try {
+            return parse(target, args);
+        } catch (IllegalArgumentException e) {
+            System.err.println(e.getMessage());
+            Args.usage(target);
+            System.exit(1);
+            throw e;
+        }
+    }
+
     /**
      * Parse a set of arguments and populate the target with the appropriate values.
      *
